a5fc0e6084b6a2cb4d6c755fe530c0ddae25534b
Pooad Boonprasertchai <theethat.boo@dome.tu.ac.th>
2026-03-20 14:48:17 +0700

    complete GET user cases API (local version)

--- server/src/configs/dynamoose.config.ts
+++ server/src/configs/dynamoose.config.ts
@@ -4,12 +4,5 @@ import dotenv from "dotenv"
 dotenv.config()
 
 export const connectDynamoDB = () => {
-  // for local development
-  if (process.env.NODE_ENV === 'development' && process.env.DYNAMODB_ENDPOINT) {
-    dynamoose.aws.ddb.local(process.env.DYNAMODB_ENDPOINT);
-    console.log('Connected to DynamoDB Local');
-  } else {
-    // for production AWS - use default AWS credentials
-    console.log('Connected to DynamoDB AWS');
-  }
+  dynamoose.aws.ddb.local(process.env.DYNAMODB_ENDPOINT!)
 }
